1f388ece5ee7539f75bbfe5b737b1ac6a5f82881
ImanliOmer <omarzi@code.edu.az>
2022-03-15 19:00:29 +0400

    upload

---
+++ HomeWork/hometask1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace hometask1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, -2, 3 };
+            Arry(ref numbers);
+            foreach (int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        static int[] Arry(ref int[] Arr)
+        {
+            for (int i = 0; i < Arr.Length; i++)
+            {
+                if (Arr[i]<0)
+                {
+                    Arr[i] *= -1;
+                }
+            }
+            return Arr;
+        }
+    }
+}
